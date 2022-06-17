42647b19697c229717017e367535f20dda83bca1
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-06-18 00:17:42 +0700

    Made contact info page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
+    const { name, phone, email, photo } = data;
     return (
-        <></>
+        <div className='Content'>
+        <div className='Contact'>
+            <img src={photo} alt={name} />
+            <div className='Contact-data'>
+                <h2> {name} </h2>
+                <p> {phone} </p>
+                <p> {email} </p>
+            </div>
+        </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <></>
+        <div className='Header'>
+            <h1> Call a Friend </h1>
+            <h3> Your Friendly Contact App </h3>
+        </div>
     )
 }
 
